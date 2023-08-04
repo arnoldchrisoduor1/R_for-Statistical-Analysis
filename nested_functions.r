3e021aebf7adba8c6252bdd726a27e3b93807c02
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-08-04 22:46:47 +0300

    recursion

---
+++ nested_functions.r
@@ -0,0 +1,20 @@
+#Created in two ways:-
+#creating a function within another function.
+#Calling another function.
+
+Nested_function <- function(x, y) {
+  a <- x + y
+  return(a)
+}
+
+Nested_function(Nested_function(2,2), Nested_function(3,3))
+
+Outer_func <- function(x) {
+  Inner_func <- function(y) {
+    a <- x + y
+    return(a)
+  }
+  return (Inner_func)
+}
+output <- Outer_func(3) # To call the Outer_func
+output(5)
