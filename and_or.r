d2b68715200dc10626a27a04eb976916a899d1a8
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-08-04 22:21:30 +0300

    and or

---
+++ and_or.r
@@ -0,0 +1,18 @@
+#and (&)
+
+a <- 200
+b <- 33
+c <- 500
+
+if (a > b & c > a) {
+  print("Both conditions are true")
+}
+
+#or |
+a <- 200
+b <- 33
+c <- 500
+
+if (a > b | a > c) {
+  print("At least one of the conditions is true")
+}
