741a326e4d5323c1c04d0e4c6ffbc5e38edd6aaa
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-08-04 22:11:03 +0300

    booleans

---
+++ booleans.r
@@ -0,0 +1,18 @@
+10 > 9    # TRUE because 10 is greater than 9
+10 == 9   # FALSE because 10 is not equal to 9
+10 < 9    # FALSE because 10 is greater than 9
+
+a <- 10
+b <- 9
+
+a > b
+
+#booleans in loops
+a <- 200
+b <- 33
+
+if (b > a) {
+  print ("b is greater than a")
+} else {
+  print("b is not greater than a")
+}
